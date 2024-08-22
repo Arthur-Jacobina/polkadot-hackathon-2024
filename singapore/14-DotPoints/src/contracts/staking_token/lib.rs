8d82e31b45278c2066251196e290b0b2b550f1af
Arthur-Jacobina <email27arthur@gmail.com>
2024-08-22 00:35:55 -0300

    add psp22 compliant token contract and remove deprecated contracts

---
+++ singapore/14-DotPoints/src/contracts/staking_token/lib.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+        
+#[openbrush::implementation(PSP22, PSP22Metadata)]
+#[openbrush::contract]
+pub mod staking_token {
+    use openbrush::traits::Storage;
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct StakingTokenContract {
+    	#[storage_field]
+		psp22: psp22::Data,
+		#[storage_field]
+		metadata: metadata::Data,
+    }
+    
+    impl StakingTokenContract {
+        #[ink(constructor)]
+        pub fn new(initial_supply: Balance, name: Option<String>, symbol: Option<String>, decimal: u8) -> Self {
+            let mut _instance = Self::default();
+			psp22::Internal::_mint_to(&mut _instance, Self::env().caller(), initial_supply).expect("Should mint"); 
+			_instance.metadata.name.set(&name);
+			_instance.metadata.symbol.set(&symbol);
+			_instance.metadata.decimals.set(&decimal);
+			_instance
+        }
+    }
+}
